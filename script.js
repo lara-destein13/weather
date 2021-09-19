44a385b4d1b41ff75b68df73f5d7b42110461da8
Lara Destein <laradestein13@gmail.com>
2021-09-18 20:18:57 -0700

    more css stuff

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
+function setInnerHTML(id, value) {
+    var element = document.getElementById(id);
+    element.innerHTML = value;
+}
+  
+function setOnClick(id, func) {
+    var element = document.getElementById(id);
+    element.onclick = func;
+}
+  
+var apiKey = 'b671a94b0a5fb7d77df8e0def5fb933b';
 
+function createAutoCompleter() {
+    var cityAndStateArray = [];
+    for (var i = 0; i < cities.length; i += 1) {
+        var city = cities[i];
+        var name = city.name;
+        var state = city.state;
+        var cityAndState = `${name}, ${state}`;
+        cityAndStateArray.push(cityAndState);
+    }
+
+    var options = {
+        source: cityAndStateArray,
+    };
+
+    $('#search-input').autocomplete(options);
+}
+
+function selected() {
+    alert('selected');
+}
+
+function searchClicked() {
+    var text = document.getElementById('search-input').value
+    alert(text);
+}
+
+function main() {
+    setOnClick('search-button', searchClicked);
+    //createAutoCompleter();
+}
+
+main();
