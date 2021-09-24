f175b2a7dec4195baeb3b51f041bf1e6fcf67bbd
Lara Destein <laradestein13@gmail.com>
2021-09-24 13:23:11 -0700

    add some comments

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
-// Note that this program uses the 
-// city information in the file cities.js. The cities file 
-// from OpenWeather was huge, so I asked a friend to create 
-// a similar file that had only US cities. It's much smaller 
-// and has 90,000 cities.
-
 var apiKey = 'b671a94b0a5fb7d77df8e0def5fb933b'
 
-// This is a simple utility function that 
-// finds a DOM element with id 'id', and sets its innerHTMLM 
-// to the value in 'value'.
+// A simple utility function that finds a DOM element with id 'id', 
+// and sets its innerHTML to the value in 'value';
 function setInnerHTML(id, value) {
     var element = document.getElementById(id);
     element.innerHTML = value;
 }
 
-// This is a simple utility function that 
-// finds a DOM element with id 'id', and attaches click 
-//handler function 'func'.
+// A simple utility function that finds a DOM element with id 'id', 
+// and assigns the click handler function 'func'.
 function setOnClick(id, func) {
     var element = document.getElementById(id);
     element.onclick = func;
 }
 
+// Configure the JQuery UI Autocompleter. First create an array of 
+// strings for the form 'City, ST'. Then tell JQuery to attach its 
+// autocompleter functionality to our city search input element. 
 function createAutoCompleter() {
     var cityAndStateArray = [];
     for (var i = 0; i < cities.length; i += 1) {
@@ -39,6 +34,9 @@ function createAutoCompleter() {
     $('#search-input').autocomplete(options);
 }
 
+// I have five days in my HTML. This function populates the five 
+// field in one day (identified by dayNumber) with data returned from 
+// the OpenWeather APIs.
 function updateDay(dayNumber, data) {
     var date = data.dt_txt.replace(/ .*/, '');
     var icon = 'icon';
@@ -59,6 +57,11 @@ function updateDay(dayNumber, data) {
     setInnerHTML(humidityId, `Humidity: ${humidity} %`);
 }
 
+// I have one div in my HTML that contains the curent weather date. This function
+// makes an API call to the OpenWeather web service. The function then populates the 
+// five fields in the current weather div with data returned from the OpenWeather
+// API. Note that this function is diclared as 'async' so that I can use 'await' to
+// stop and wait for the response to our API call. 
 async function getCurrentWeather(city, state, id) {
     var url = `https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${apiKey}&units=imperial`;
     var response = await fetch(url);
@@ -78,6 +81,9 @@ async function getCurrentWeather(city, state, id) {
     setInnerHTML('uv', `UV Index: ___`);
 }
 
+// This function makes an APU call to the OpenWeather web service. Note that this 
+// function is declared as 'async' so that I can use 'await' to stop and wait for
+// the response to our API call. 
 async function getForecast(city, state, id) {
     var url = `https://api.openweathermap.org/data/2.5/forecast?id=${id}&appid=${apiKey}&units=imperial`;
     var response = await fetch(url);
@@ -95,6 +101,13 @@ async function getForecast(city, state, id) {
     }
 }
 
+// This function is called when the user clicks the city 'search' button, or when
+// the user clicks on one of the five city buttons. It manages our list of the five 
+// most recently queried cities. I store that list in localStorage. I'm careful to make 
+// sure that my list always contains five cities or the non-breaking space character 
+// (&nbsp;). Note that before adding a new city to our list, I remove it from the list if 
+// its already there. Once I have updated the list, I update the innerHTML of the five 
+// city buttons. 
 function updateButtons(cityAndState) {
     var cities = localStorage.getItem('cities')
 
@@ -132,7 +145,11 @@ function updateButtons(cityAndState) {
 }
 
 
-
+// This function is called when the user selects a city wither using the input
+// element or one of the city buttons. It is passed a string of the form 
+// 'City, ST'. It needs to find the corresponding city object in our cities 
+// array so that I can know the ID for the selected city. Then it passes that
+// ID to the functions that make the API calls to OpenWeather.
 function showWeatherForCity(selectedCityAndState) {
     updateButtons(selectedCityAndState);
     for (let i = 0; i < cities.length; i++) {
@@ -148,17 +165,23 @@ function showWeatherForCity(selectedCityAndState) {
     }
 }
 
+// This is the click handler function for our 'Search' button. It gets the city
+// and state from the input field, and calls a function to get data for that
+// city and state and update the DOM accordingly.
 function searchClicked() {
     var cityAndState = document.getElementById('search-input').value;
     showWeatherForCity(cityAndState);
 }
 
-function main() {
-    createAutoCompleter();
-    setOnClick('search-button', searchClicked);
-    showWeatherForCity('Nashville, TN');
-}
+// function main() {
+//     createAutoCompleter();
+//     setOnClick('search-button', searchClicked);
+//     showWeatherForCity('Nashville, TN');
+// }
 
+// This function is called when the user clicks on one of the five city buttons. 
+// It gets the list of five 'City, ST' strings from local storage and calls a 
+// function to get data for that city and state and update the DOM accordingly.
 function buttonClicked(number) {
     var cities = localStorage.getItem('cities')
     cities = JSON.parse(cities);
@@ -166,6 +189,7 @@ function buttonClicked(number) {
     showWeatherForCity(cityAndState);
 }
 
+// These are the click hanlers for the five city buttons. 
 function button0() {
     buttonClicked(0);
 }
@@ -187,13 +211,18 @@ function button4() {
 }
 
 function main() {
+    // Initialize the JQuery autocompleter
     createAutoCompleter();
+
+    // Attach some button click handler functions
     setOnClick('search-button', searchClicked);
     setOnClick('button0', button0);
     setOnClick('button1', button1);
     setOnClick('button2', button2);
     setOnClick('button3', button3);
     setOnClick('button4', button4);
+
+    // Show Nashville weather to start with.
     showWeatherForCity('Nashville, TN');
 }
 
