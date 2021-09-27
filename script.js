1269fb3703376fe5d126a944ac852132f0d2dcd9
Lara Destein <laradestein13@gmail.com>
2021-09-26 18:40:52 -0700

    include uv index

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var apiKey = 'b671a94b0a5fb7d77df8e0def5fb933b';
-var autocomplete = null;
-
+var localStorageKey = 'cities-4';
+    
 // A simple utility function that finds a DOM element with id 'id', 
 // and sets its innerHTML to the value in 'value';
 function setInnerHTML(id, value) {
@@ -31,73 +31,6 @@ function createAutoCompleter() {
     $('#search-input').autocomplete({ source: options });
 }
 
-// I have five days in my HTML. This function populates the five 
-// field in one day (identified by dayNumber) with data returned from 
-// the OpenWeather APIs.
-function updateDay(dayNumber, data) {
-    var date = data.dt_txt.replace(/ .*/, '');
-    var icon = 'icon';
-    var temp = data.main.temp;
-    var wind = data.wind.speed;
-    var humidity = data.main.humidity;
-
-    var dateId = `day-${dayNumber}-date`
-    var iconId = `day-${dayNumber}-icon`
-    var tempId = `day-${dayNumber}-temp`
-    var windId = `day-${dayNumber}-wind`
-    var humidityId = `day-${dayNumber}-humidity`
-
-    setInnerHTML(dateId, date);
-    setInnerHTML(iconId, icon);
-    setInnerHTML(tempId, `Temp: ${temp} F`);
-    setInnerHTML(windId, `Wind: ${wind} MPH`);
-    setInnerHTML(humidityId, `Humidity: ${humidity} %`);
-}
-
-// I have one div in my HTML that contains the curent weather date. This function
-// makes an API call to the OpenWeather web service. The function then populates the 
-// five fields in the current weather div with data returned from the OpenWeather
-// API. Note that this function is diclared as 'async' so that I can use 'await' to
-// stop and wait for the response to our API call. 
-async function getCurrentWeather(city, state, id) {
-    var url = `https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${apiKey}&units=imperial`;
-    var response = await fetch(url);
-    response = await response.json();
-    console.log('current weather:');
-    console.log(JSON.stringify(response, null, 4));
-
-    var temp = response.main.temp;
-    var wind = response.wind.speed;
-    var humidity = response.main.humidity;
-    var uv = 'uv';
-
-    setInnerHTML('city', `${city}, ${state}`);
-    setInnerHTML('temp', `Temp: ${temp} F`);
-    setInnerHTML('wind', `Wind: ${wind} MPH`);
-    setInnerHTML('humidity', `Humidity: ${humidity} %`);
-    setInnerHTML('uv', `UV Index: ___`);
-}
-
-// This function makes an APU call to the OpenWeather web service. Note that this 
-// function is declared as 'async' so that I can use 'await' to stop and wait for
-// the response to our API call. 
-async function getForecast(city, state, id) {
-    var url = `https://api.openweathermap.org/data/2.5/forecast?id=${id}&appid=${apiKey}&units=imperial`;
-    var response = await fetch(url);
-    response = await response.json();
-
-    var list = response.list;
-    var dayNumber = 0;
-    for (var i = 0; i < list.length; i ++) {
-        var listItem = list[i];
-        var dateTime = listItem.dt_txt;
-        if (dateTime.includes('12:00:00')) {
-            updateDay(dayNumber, listItem);
-            dayNumber += 1;
-        }
-    }
-}
-
 // This function is called when the user clicks the city 'search' button, or when
 // the user clicks on one of the five city buttons. It manages our list of the five 
 // most recently queried cities. I store that list in localStorage. I'm careful to make 
@@ -106,29 +39,27 @@ async function getForecast(city, state, id) {
 // its already there. Once I have updated the list, I update the innerHTML of the five 
 // city buttons. 
 function updateButtons(cityAndState) {
-    var cities = localStorage.getItem('cities')
+    // Get the list of five cities from localStorage.  If there are no cities in
+    // local storage, create a list of five empty strings.
 
+    var cities = localStorage.getItem(localStorageKey)
     if (cities != null) {
         cities = JSON.parse(cities);
     } else {
-        cities = [];
+        cities = ['&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp' ];
     }
+
+    // If the selected city is one of the five from local storage, remove it.
 
     for (var i = 0; i < cities.length; i++) {
         if (cities[i] === cityAndState) {
             cities.splice(i, 1);
+            cities.push('&nbsp');
         }
     }
 
     cities.unshift(cityAndState);
-
-    while (cities.length < 5) {
-        cities.push('&nbsp;');
-    }
-
-    while (cities.length > 5) {
-        cities.pop();
-    }
+    cities.pop();
 
     for (var i = 0; i < 5; i++) {
         var id = `button${i}`;
@@ -136,11 +67,62 @@ function updateButtons(cityAndState) {
     }
 
     cities = JSON.stringify(cities);
-    console.log(cities);
-
-    localStorage.setItem('cities', cities);
+    localStorage.setItem(localStorageKey, cities);
 }
 
+// This function updates the innerHTML in the current weather div.
+function setCurrentWeather(city, state, weather) {
+    var temp = weather.temp;
+    var wind = weather.wind_speed;
+    var humidity = weather.humidity;
+    var uvi = weather.uvi;
+    
+    setInnerHTML('city', `${city}, ${state}`);
+    setInnerHTML('temp', `Temp: ${temp} F`);
+    setInnerHTML('wind', `Wind: ${wind} MPH`);
+    setInnerHTML('humidity', `Humidity: ${humidity} %`);
+    setInnerHTML('uv', `UV Index: ${uvi}`);
+}
+
+// The HTMl has five 'day' divs.  This function is called to update
+// the InnerHTML in one of those divs.  The function is passed the
+// day number (0 to 4), and the weather for that day.
+function setDailyWeather(dayNumber, weather) {
+    var date = new Date(weather.dt * 1000);
+    var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    var day = days[date.getDay()];
+    
+    var icon = 'icon';
+    var temp = weather.temp.day;
+    var wind = weather.wind_speed;
+    var humidity = weather.humidity;
+
+    var dateId = `day-${dayNumber}-date`
+    var iconId = `day-${dayNumber}-icon`
+    var tempId = `day-${dayNumber}-temp`
+    var windId = `day-${dayNumber}-wind`
+    var humidityId = `day-${dayNumber}-humidity`
+
+    setInnerHTML(dateId, day);
+    setInnerHTML(iconId, icon);
+    setInnerHTML(tempId, `Temp: ${temp} F`);
+    setInnerHTML(windId, `Wind: ${wind} MPH`);
+    setInnerHTML(humidityId, `Humidity: ${humidity} %`);
+}
+
+// Make an API call to the OpenWeather OneCall service.  Note that this
+// function is declared as 'async.'  This way I can use 'await' to stop
+// and wait for the API call to complete.  When I get the response, I
+// extract the current weather and forecast from the response.
+async function getWeather(name, state, id, lat, lon) {
+    var url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${apiKey}&units=imperial`;
+    var response = await fetch(url);
+    response = await response.json();
+    setCurrentWeather(name, state, response.current);
+    for (var i = 0; i < 5; i ++) {
+        setDailyWeather(i, response.daily[i]);
+    }
+}
 
 // This function is called when the user selects a city wither using the input
 // element or one of the city buttons. It is passed a string of the form 
@@ -148,16 +130,17 @@ function updateButtons(cityAndState) {
 // array so that I can know the ID for the selected city. Then it passes that
 // ID to the functions that make the API calls to OpenWeather.
 function showWeatherForCity(selectedCityAndState) {
-    updateButtons(selectedCityAndState);
-    for (let i = 0; i < cities.length; i++) {
+    for (var i = 0; i < cities.length; i++) {
         var city = cities[i];
-        var name = city.name;
         var id = city.id;
+        var lat = city.coord.lat;
+        var lon = city.coord.lon;
+        var name = city.name;
         var state = city.state;
         var cityAndState = `${name}, ${state}`;
         if (selectedCityAndState === cityAndState) {
-            getCurrentWeather(name, state, id);
-            getForecast(name, state, id);
+            getWeather(name, state, id, lat, lon);
+            updateButtons(selectedCityAndState);
         }
     }
 }
@@ -170,17 +153,11 @@ function searchClicked() {
     showWeatherForCity(cityAndState);
 }
 
-// function main() {
-//     createAutoCompleter();
-//     setOnClick('search-button', searchClicked);
-//     showWeatherForCity('Nashville, TN');
-// }
-
 // This function is called when the user clicks on one of the five city buttons. 
 // It gets the list of five 'City, ST' strings from local storage and calls a 
 // function to get data for that city and state and update the DOM accordingly.
 function buttonClicked(number) {
-    var cities = localStorage.getItem('cities')
+    var cities = localStorage.getItem(localStorageKey)
     cities = JSON.parse(cities);
     cityAndState = cities[number];
     showWeatherForCity(cityAndState);
